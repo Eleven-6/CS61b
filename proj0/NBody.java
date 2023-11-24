649a25b30d6f239a5007719621910020a826181c
Eleven-6 <2824935478@qq.com>
2023-11-25 07:47:15 +0800

    Complete project0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -24,21 +24,49 @@ public class NBody {
     }
 
     public static void main(String[] args) {
-        double T = Double.parseDouble(args[0]);
-        double dt = Double.parseDouble(args[1]);
+        double T = new Double(args[0]);
+        double dt = new Double(args[1]);
         String filename = args[2];
         double r = readRadius(filename);
         Planet[] planets = readPlanets(filename);
 
+        // set the universe scale
         StdDraw.setXscale(-r, r);
         StdDraw.setYscale(-r, r);
-        StdDraw.picture(0, 0, "images/starfield.jpg");
         StdDraw.enableDoubleBuffering();
 
-        for (Planet p:planets) {
-            p.draw();
+        double t = 0;
+        int num = planets.length;
+        while(t <= T){
+            double[] xForces = new double[num];
+            double[] yForces = new double[num];
+            for(int i = 0; i < num; i++){
+                xForces[i] = planets[i].calcNetForceExertedByX(planets);
+                yForces[i] = planets[i].calcNetForceExertedByY(planets);
+            }
+            for(int i = 0; i < num; i++){
+                planets[i].update(dt, xForces[i], yForces[i]);
+            }
+
+            // draw the backgroud picture
+            StdDraw.picture(0, 0, "images/starfield.jpg");
+
+            // draw all the planets
+            for (Planet planet : planets) {
+                planet.draw();
+            }
+
+            StdDraw.show();
+            StdDraw.pause(10);
+            t += dt;
         }
-        StdDraw.show();
-        StdDraw.pause(2000);
+
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", r);
+        for (Planet planet : planets) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                    planet.xxPos, planet.yyPos, planet.xxVel,
+                    planet.yyVel, planet.mass, planet.imgFileName);
+        }
     }
 }
